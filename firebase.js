b018092bd93f8e2b932417c251590b50eb8a2c30
VickerTan <vickerpangestan@gmail.com>
2023-08-03 21:56:20 +0800

    Create firebase config

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBf3QrrfborN6TLxTSM6Y8p9EMdkSztgEw",
+  authDomain: "question101.firebaseapp.com",
+  projectId: "question101",
+  storageBucket: "question101.appspot.com",
+  messagingSenderId: "140279306236",
+  appId: "1:140279306236:web:2a13f4678600d21ea60479",
+  measurementId: "G-FSR6S7NZ8L",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
