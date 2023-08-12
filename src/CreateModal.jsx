38ed8db35d61e5fe4f4f0548109e32a3c0368619
VickerTan <vickerpangestan@gmail.com>
2023-08-12 22:06:18 +0800

    Create modal on Create field

---
+++ src/CreateModal.jsx
@@ -0,0 +1,52 @@
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Typography from "@mui/material/Typography";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function TransitionsModal({ open, handleClose }) {
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Create Field
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              {
+                "User can submit their question(s) here, and the submitted question(s) will be shared across all users."
+              }
+              {"\nContent :"}
+              {"\nCategory : "}
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
