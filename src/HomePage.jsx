00be8e8706c826bb2472bb264a260fe9a46f6755
VickerTan <vickerpangestan@gmail.com>
2023-08-12 22:06:56 +0800

    Import MenuSelection

---
+++ src/HomePage.jsx
@@ -0,0 +1,10 @@
+import MenuSelection from "./MenuSelection";
+
+export default function HomePage() {
+  return (
+    <div className="home-page" style={{ textAlign: "center" }}>
+      <h1>Question 101</h1>
+      <MenuSelection />
+    </div>
+  );
+}
