64558f895d3b61bbe155e6f4dbd4a8b089b71d19
VickerTan <vickerpangestan@gmail.com>
2023-08-25 22:59:10 +0800

    Store components inside Box
    Import AppBar

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,10 +1,17 @@
+import { Box } from "@mui/material";
 import MenuSelection from "./MenuSelection";
+import AppBar from "./AppBar";
 
 export default function HomePage() {
   return (
-    <div className="home-page" style={{ textAlign: "center" }}>
+    <Box
+      sx={{
+        textAlign: "center",
+      }}
+    >
+      <AppBar />
       <h1>Question 101</h1>
       <MenuSelection />
-    </div>
+    </Box>
   );
 }
