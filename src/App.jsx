5988e72fc87d750a82c5d9a52bd6098cc3e29fd4
VickerTan <vickerpangestan@gmail.com>
2023-08-27 22:42:44 +0800

    Rerender app on account changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import "./style.css";
 import HomePage from "./HomePage";
+import { useState, useEffect } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
   console.log("Render app");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (u) => {
+      if (u) {
+        setUser(u);
+      } else {
+        setUser(null);
+      }
+    });
+  });
+
+  console.log(user ? user.displayName : "No user");
 
   return <HomePage />;
 }
