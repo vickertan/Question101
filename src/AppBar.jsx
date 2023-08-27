561f234d319b5a515befe9ac61dec0d56de3c913
VickerTan <vickerpangestan@gmail.com>
2023-08-27 22:41:31 +0800

    Add button to Sign In with Google

--- src/AppBar.jsx
+++ src/AppBar.jsx
@@ -1,15 +1,30 @@
 import { Box, Button } from "@mui/material";
+import { auth, googleProvider } from "../firebase";
+import { signInWithRedirect } from "firebase/auth";
 
 export default function ButtonAppBar() {
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithRedirect(auth, googleProvider);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Box
       sx={{
         backgroundColor: "lightpink",
         display: "flex",
-        flex: "1 1 0%",
       }}
     >
-      <Button sx={{ ml: "auto" }}>Log In</Button>
+      {auth.currentUser ? (
+        <Button sx={{ ml: "auto" }}>Icon here</Button>
+      ) : (
+        <Button sx={{ ml: "auto" }} onClick={signInWithGoogle}>
+          Log In
+        </Button>
+      )}
     </Box>
   );
 }
