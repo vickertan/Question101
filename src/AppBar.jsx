1e0277b052258129fc24c95dfdf7fe10cdf344fd
VickerTan <vickerpangestan@gmail.com>
2023-08-28 17:20:02 +0800

    Display user icon when logged in
    Create Log Out button

--- src/AppBar.jsx
+++ src/AppBar.jsx
@@ -1,6 +1,9 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, IconButton, Menu, MenuItem, Avatar } from "@mui/material";
 import { auth, googleProvider } from "../firebase";
 import { signInWithRedirect } from "firebase/auth";
+import { useState } from "react";
+
+const settings = ["Log Out"];
 
 export default function ButtonAppBar() {
   const signInWithGoogle = async () => {
@@ -11,17 +14,58 @@ export default function ButtonAppBar() {
     }
   };
 
+  function UserIcon() {
+    const [anchorElUser, setAnchorElUser] = useState(null);
+
+    const handleOpenUserMenu = (e) => {
+      setAnchorElUser(e.currentTarget);
+    };
+
+    const handleCloseUserMenu = () => {
+      setAnchorElUser(null);
+    };
+
+    return (
+      <Box sx={{ ml: "auto" }}>
+        <IconButton onClick={handleOpenUserMenu} size="small">
+          <Avatar alt="Remy Sharp" src={auth.currentUser.photoURL} />
+        </IconButton>
+        <Menu
+          sx={{ mt: "45px" }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          <MenuItem key="Log Out" onClick={() => auth.signOut()}>
+            Log Out
+          </MenuItem>
+        </Menu>
+      </Box>
+    );
+  }
+
   return (
     <Box
       sx={{
         backgroundColor: "lightpink",
         display: "flex",
+        height: "fill-content",
       }}
     >
       {auth.currentUser ? (
-        <Button sx={{ ml: "auto" }}>Icon here</Button>
+        <UserIcon />
       ) : (
-        <Button sx={{ ml: "auto" }} onClick={signInWithGoogle}>
+        <Button sx={{ ml: "auto", height: "50px" }} onClick={signInWithGoogle}>
           Log In
         </Button>
       )}
