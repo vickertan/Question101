03cb8779b70cc8ff4ac504e20d112289975e2bd7
VickerTan <vickerpangestan@gmail.com>
2023-08-29 20:06:41 +0800

    Add new selection in menu

--- src/MenuSelection.jsx
+++ src/MenuSelection.jsx
@@ -1,7 +1,5 @@
 import { useRef } from "react";
-import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
-import Box from "@mui/material/Box";
+import { Button, ButtonGroup, Box } from "@mui/material";
 import CreateModal from "./CreateModal";
 
 export default function MenuSelection() {
@@ -23,7 +21,15 @@ export default function MenuSelection() {
         aria-label="vertical contained button group"
         variant="text"
       >
-        <Button>Playground</Button>
+        <Button
+          sx={{ border: "solid", height: "10rem", width: "10rem", mb: 2 }}
+        >
+          Card Icon here
+        </Button>
+        <Button>Topic</Button>
+        <Button>Favorite</Button>
+
+        {/* Only show Create menu selection for verified user */}
         <Button onClick={handleOpen}>Create</Button>
       </ButtonGroup>
       <CreateModal ref={modalRef} />
