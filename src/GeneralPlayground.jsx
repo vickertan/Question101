272ec0f6f27e65c9742fa4a56b93196a1c104dc6
VickerTan <vickerpangestan@gmail.com>
2023-09-12 12:47:41 +0800

    Add functions

--- src/GeneralPlayground.jsx
+++ src/GeneralPlayground.jsx
@@ -1,4 +1,11 @@
-import { useContext, useEffect, useState } from "react";
+import {
+  useContext,
+  useEffect,
+  useState,
+  useMemo,
+  useRef,
+  createRef,
+} from "react";
 import QuestionCollContext from "./QuestionCollContext";
 import { getDocs } from "firebase/firestore";
 import TinderCard from "react-tinder-card";
@@ -25,11 +32,66 @@ export default function GeneralPlayground() {
     fetchQuestions();
   }, []);
 
+  const [currentIndex, setCurrentIndex] = useState(questionList.length - 1);
+  const [lastDirection, setLastDirection] = useState();
+  // used for outOfFrame closure
+  const currentIndexRef = useRef(currentIndex);
+
+  const childRefs = useMemo(
+    () =>
+      Array(questionList.length)
+        .fill(0)
+        .map((i) => createRef()),
+    []
+  );
+
+  const updateCurrentIndex = (val) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
+  };
+
+  const canGoBack = currentIndex < questionList.length - 1;
+
+  const canSwipe = currentIndex >= 0;
+
+  // set last direction and decrease current index
+  const swiped = (direction, nameToDelete, index) => {
+    setLastDirection(direction);
+    updateCurrentIndex(index - 1);
+  };
+
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current);
+    // handle the case in which go back is pressed before card goes outOfFrame
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+    // TODO: when quickly swipe and restore multiple times the same card,
+    // it happens multiple outOfFrame events are queued and the card disappear
+    // during latest swipes. Only the last outOfFrame event should be considered valid
+  };
+
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < questionList.length) {
+      await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
+    }
+  };
+
+  // increase current index and show card
+  const goBack = async () => {
+    if (!canGoBack) return;
+    const newIndex = currentIndex + 1;
+    updateCurrentIndex(newIndex);
+    await childRefs[newIndex].current.restoreCard();
+  };
+
   return (
     <div className="card-area">
-      {questionList.map((question) => (
+      {questionList.map((question, index) => (
         <div className="card-container" key={question.id}>
-          <TinderCard className="card">
+          <TinderCard
+            className="card"
+            onSwipe={(dir) => swiped(dir, question.question, index)}
+            onCardLeftScreen={() => outOfFrame(question.question, index)}
+          >
             <h2 className="question">{question.question}</h2>
             <p className="category">{question.category}</p>
           </TinderCard>
