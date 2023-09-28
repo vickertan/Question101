f14e784c4dc6edab21daf78c4ba466b32e7cdbbf
VickerTan <vickerpangestan@gmail.com>
2023-09-28 21:43:07 +0800

    Create component's style in other file

---
+++ src/componentStyle.js
@@ -0,0 +1,19 @@
+const modalBoxStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "70vw",
+  maxWidth: "400px",
+  bgcolor: "beige",
+  border: "none",
+  borderRadius: "1rem",
+  boxShadow: 24,
+  p: 4,
+};
+
+const buttonStyle = {
+  m: "auto",
+};
+
+export { modalBoxStyle, buttonStyle };
