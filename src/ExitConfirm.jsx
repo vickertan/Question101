8ebc7b82398e5c470f4f4317d06c61e8d38230ce
VickerTan <vickerpangestan@gmail.com>
2023-10-01 22:32:39 +0800

    Enhance function name

--- src/ExitConfirm.jsx
+++ src/ExitConfirm.jsx
@@ -2,20 +2,23 @@ import { useState, useImperativeHandle, forwardRef } from "react";
 import { Modal, Fade, Box, ButtonGroup, Button } from "@mui/material";
 import { modalBoxStyle, buttonStyle } from "./componentStyle";
 
-const ExitConfirm = forwardRef(function ExitConfirm({ handleClose }, ref) {
-  const [exitConfirmOpen, setExitConfirmOpen] = useState(false);
+const ExitConfirm = forwardRef(function ExitConfirm(
+  { closeModalTemplate },
+  ref
+) {
+  const [exitConfirm, setExitConfirm] = useState(false);
 
-  const handleCloseConfirm = () => setExitConfirmOpen(false);
+  const closeExitConfirm = () => setExitConfirm(false);
 
   useImperativeHandle(ref, () => ({
-    handleOpenConfirm: () => {
-      setExitConfirmOpen(true);
+    openExitConfirm: () => {
+      setExitConfirm(true);
     },
   }));
 
   return (
-    <Modal open={exitConfirmOpen}>
-      <Fade in={exitConfirmOpen}>
+    <Modal open={exitConfirm}>
+      <Fade in={exitConfirm}>
         <Box sx={{ ...modalBoxStyle, width: 200 }}>
           <p>Unsaved data will be lost. Are you sure to exit?</p>
           <ButtonGroup
@@ -28,13 +31,13 @@ const ExitConfirm = forwardRef(function ExitConfirm({ handleClose }, ref) {
               sx={buttonStyle}
               color="warning"
               onClick={() => {
-                handleClose();
-                handleCloseConfirm();
+                closeModalTemplate();
+                closeExitConfirm();
               }}
             >
               Yes
             </Button>
-            <Button sx={buttonStyle} onClick={handleCloseConfirm}>
+            <Button sx={buttonStyle} onClick={closeExitConfirm}>
               No
             </Button>
           </ButtonGroup>
