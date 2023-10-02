0c1f3fdd43cbbf664304895f80a0be782aa5ad0f
VickerTan <vickerpangestan@gmail.com>
2023-10-02 10:33:30 +0800

    Create TopicForm component

---
+++ src/TopicForm.jsx
@@ -0,0 +1,10 @@
+import { forwardRef } from "react";
+
+const TopicForm = forwardRef((props, ref) => {
+  return (
+    <>
+      <h1>Select Topic</h1>
+    </>
+  );
+});
+export default TopicForm;
